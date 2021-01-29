86db3420a5db0c07988bef165a080675294e7e7a
DESKTOP-KH7DB9P\matha <techchintu2017@gmail.com>
2021-01-28 17:00:15 -0800

    Organized Project

---
+++ Zombie_FPS/Assets/_Scripts/AI/EnemyAI.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAIOverhaul : MonoBehaviour
+{
+    NavMeshAgent nm;
+
+    HealthController healthController;
+
+    public Transform target;
+    public Animator animator;
+
+    public float distanceTreshold = 10f;
+    public float attackTreshold = 1.5f;
+    
+    // Creates a drop-down menu and lets ud determine the state
+    public enum AIState { idle, chasing, attack };
+    public AIState aiState = AIState.idle;
+
+    void Start()
+    {
+        nm = GetComponent<NavMeshAgent>();
+        healthController = GetComponent<HealthController>();
+        animator = GetComponent<Animator>();
+        
+        StartCoroutine(Think());
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        float dist = Vector3.Distance(target.position, transform.position);
+    }
+
+    // Co-Routine - Zombie adapts to the player location
+    public IEnumerator Think()
+    {
+        while(true)
+        {
+            switch (aiState)
+            {
+                case AIState.idle:
+
+                    float dist = Vector3.Distance(target.position, transform.position);
+
+                    if (dist < distanceTreshold) // if dist is less than 10 chase the player
+                    {
+                       aiState = AIState.chasing; 
+                       animator.SetBool("chasing", true);
+                    }
+
+                    nm.SetDestination(transform.position);
+                    break;
+
+                case AIState.chasing:
+
+                    dist = Vector3.Distance(target.position, transform.position);
+
+                    if (dist > distanceTreshold) // if dist is more than 10 become idle
+                    {
+                        aiState = AIState.idle;
+                        animator.SetBool("chasing", false);
+                    }
+
+                    if(dist < attackTreshold) // if dist is less than 1.5 attack player
+                    {
+                        aiState = AIState.attack;
+                        animator.SetBool("attacking", true);
+                    }
+                    nm.SetDestination(target.position);
+                    break;
+
+                case AIState.attack: 
+                    //Debug.Log("Attack");
+                    dist = Vector3.Distance(target.position, transform.position);
+                    if (dist > attackTreshold) // if dist is more than 1.5 chase player
+                    {
+                        aiState = AIState.chasing;
+                        animator.SetBool("attacking", false);
+                    }
+
+                    nm.SetDestination(transform.position);
+                    break;
+
+                default:
+                    break;
+            }
+
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+
+    void OnTriggerStay(Collider collider) 
+    {
+        collider.gameObject.GetComponent<PlayerHealth>().TakeDamage(1);
+    }
+
+
+}
---
+++ Zombie_FPS/Assets/_Scripts/AI/PatrolPath.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PatrolPath : MonoBehaviour
+{  
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private Transform target;
+
+    [SerializeField] private float distanceTreshold = 15f;
+    [SerializeField] private float maxSpeed = 10f;
+    [SerializeField] private float minSpeed = 3f;
+
+    [SerializeField] private enum AiStates {Patrol, Follow, Attack}
+    [SerializeField] AiStates currentState;
+
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private int curentWaypoint;
+
+    private void Start()
+    {
+        if(agent == null)
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+    }
+
+    private void Update()
+    {
+        UpdateStates();
+    }
+
+    private void UpdateStates()
+    {   
+        switch (currentState)
+        {
+            case AiStates.Patrol:
+                Patrol();
+                break;
+        }
+    }
+
+    private void Patrol()
+    {   
+        // If the waypoints are not being followed, then the agent should follow them
+        if(agent.destination != waypoints[curentWaypoint].position)
+        {
+            agent.destination = waypoints[curentWaypoint].position;
+        }
+
+        // Incremnting waypoints, getting the length of it and if we reach the end of it, set waypoints to 0 and restart 
+        if(hasReached())
+        {
+            curentWaypoint = (curentWaypoint + 1) % waypoints.Length;
+        }
+
+        // Distance From the player and the AI
+        float dist = Vector3.Distance(target.position, transform.position);
+
+        // Follow the player if they get too close
+        if(dist < distanceTreshold)
+        {
+            transform.LookAt(target);
+            agent.SetDestination(target.position); 
+            agent.speed = maxSpeed;
+        }
+        else
+        {
+            agent.speed = minSpeed;
+        }
+    }
+
+    // Returns true if the path is not being persued and if the distance to the path is less than stopping distance
+    private bool hasReached()
+    {
+        return(agent.hasPath && !agent.pathPending && agent.remainingDistance <= agent.stoppingDistance);
+    }
+}
